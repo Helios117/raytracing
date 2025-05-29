23f072b71afdad407c4c1b50634547afaa92f231
Suraj Vaidyanathan <suraj.vaidyanathan@gmail.com>
2025-05-29 17:51:39 +0530

    Added progress indicator

--- main.cc
+++ main.cc
@@ -7,10 +7,11 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
-            auto b = 0;
+            auto b = 100;
 
             int ir =  int(255.999 * r);
             int ig =  int(255.999 * g);
@@ -19,4 +20,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone.                 \n";
 }
